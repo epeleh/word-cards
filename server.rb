ddb72b6a4bc55e320d124cf73f1d7c0b286d000d
Evgeny Peleh <pelehev@gmail.com>
2022-01-20 20:51:22 +0300

    Add cards api

--- server.rb
+++ server.rb
@@ -31,10 +31,41 @@ end
 
 require 'sinatra'
 require 'sinatra/json'
+require 'sinatra/namespace'
 
 set :environment, 'production'
 set :public_folder, File.join(__dir__, 'dist')
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
+end
+
+namespace '/api' do
+  before { content_type :json }
+
+  namespace '/cards' do
+    get do
+      json DB[:cards].all
+    end
+
+    get '/:id' do
+      card = DB[:cards][id: params[:id]]
+      halt(404) if card.nil?
+      json(card)
+    end
+
+    post do
+    end
+
+    put '/:id' do
+    end
+
+    delete '/:id' do
+      card = DB[:cards][id: params[:id]]
+      halt(404) if card.nil?
+      DB[:cards].where(id: params[:id]).delete == 1 ? halt(200) : halt(400)
+    end
+  end
+
+  not_found { '' }
 end
